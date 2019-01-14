625987a8cc65b335bfbca6660b6bddbbe0f668a5
conduongsang <nguyentathuy1986@gmail.com>
2019-01-14 23:29:28 +0700

    eight

--- application/frontend/modules/lookupinvoice/views/list.php
+++ application/frontend/modules/lookupinvoice/views/list.php
@@ -4,7 +4,10 @@ foreach ($rows as $iv) {
     echo "<tr>";
     echo '<td><span class="ipdf" itc="' . $iv['templateCode'] . '" ino="' . $iv['invoiceNo'] . '" iid="' . $iv['invoiceId'] . '">pdf</span> | <span class="izip" itc="' . $iv['templateCode'] . '" ino="' . $iv['invoiceNo'] . '" iid="' . $iv['invoiceId'] . '">zip</span></td>';
     echo '<td><span class="viewpdf" itc="' . $iv['templateCode'] . '" ino="' . $iv['invoiceNo'] . '" iid="' . $iv['invoiceId'] . '">View</span></td>';
-    foreach ($iv as $v) {
+    foreach ($iv as $k=>$v) {
+    	if($k=='createTime') {
+    		$v = date("d-m-Y H:i", 1388516401);
+    	}
         echo "<td>$v</td>";
     }
     echo "<tr>";
