e46dfea7509e8649d44b34703cb073f94614b8ca
cdsvn <conduongsang.info@gmail.com>
2019-03-26 22:02:15 +0700

    modify account

--- application/config/account.php
+++ application/config/account.php
@@ -1,15 +1,24 @@
 <?php
-    // Test
-//    $config['purl'] = 'https://demo-sinvoice.viettel.vn:8443';
-//    $config['pport'] = '8443';
-//    $config['infoByerIdNo'] = array(
-//        array('0311114017', '0311114017', 'Test@123456'),
-//        array('0100109106-997', '0100109106-997', '123456a@A')
-//    );
 
+    // Test
+    //    $config['purl'] = 'https://demo-sinvoice.viettel.vn:8443';
+    //    $config['pport'] = '8443';
+    //    $config['infoByerIdNo'] = array(
+    //        array('0311114017', '0311114017', 'Test@123456'),
+    //        array('0100109106-997', '0100109106-997', '123456a@A')
+    //    );
+    
     // Product
     $config['purl'] = 'https://api-sinvoice.viettel.vn:443';
     $config['pport'] = '443';
     $config['infoByerIdNo'] = array(
-        "0311114017" => array('0311114017', '0311114017_portal', '111111a@A', array('0311114017-004'=>array('0311114017-004_portal', '111111a@A'), '0311114017-005'=>array('0311114017-005_portal', '111111a@A')))
+        "0311114017" => array(
+            '0311114017',
+            '0311114017_portal',
+            '111111a@A',
+            array(
+                '0311114017-004' => array('0311114017-004_portal', '111111a@A'),
+                '0311114017-005' => array('0311114017-005_portal', '111111a@A')
+            )
+        )
     );
